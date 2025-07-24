90297bf2deeab5aa7d4518ff714a0cee3447bf2e
Timo Janssen <47943378+tijanssen@users.noreply.github.com>
2025-07-24 20:36:01 +0200

    Fixes

--- tresty-collections/src/main/java/io/github/tresty/collections/internal/view/ReverseMutableListView.java
+++ tresty-collections/src/main/java/io/github/tresty/collections/internal/view/ReverseMutableListView.java
@@ -1,3 +1,28 @@
+/*-
+ * #%L
+ * tresty-collections
+ * %%
+ * Copyright (C) 2025 Timo Janssen
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
 package io.github.tresty.collections.internal.view;
 
 import java.util.function.Predicate;
@@ -126,30 +151,30 @@ public final class ReverseMutableListView<E> implements MutableList<E> {
     @Override
     public void add(Collection<? extends E> c) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void add(java.util.Collection<? extends E> c) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void removeIf(Predicate<? super Object> predicate) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void removeFirst() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void removeLast() {
         // TODO Auto-generated method stub
-        
+
     }
 }
